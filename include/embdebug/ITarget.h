d7f2a9d14e3ec94e88b37f7bdbb11b0df541370b
Simon Cook <simon.cook@embecosm.com>
2019-03-21 10:03:41 +0000

    Change copyright headers to use SPDX identifiers

--- include/embdebug/ITarget.h
+++ include/embdebug/ITarget.h
@@ -1,23 +1,10 @@
 // Generic GDB RSP server interface: declaration
-
-// Copyright (C) 2008-2017  Embecosm Limited <www.embecosm.com>
-
-// Contributor Jeremy Bennett <jeremy.bennett@embecosm.com>
-
-// This file is part of the RISC-V GDB server
-
-// This program is free software: you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the Free
-// Software Foundation, either version 3 of the License, or (at your option)
-// any later version.
-
-// This program is distributed in the hope that it will be useful, but WITHOUT
-// ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or
-// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-// more details.
-
-// You should have received a copy of the GNU General Public License along with
-// this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+// This file is part of the Embecosm GDB Server.
+//
+// Copyright (C) 2008-2019 Embecosm Limited
+// SPDX-License-Identifier: GPL-3.0-or-later
+// ----------------------------------------------------------------------------
 
 #ifndef ITARGET_H
 #define ITARGET_H
--- server/VContActions.cpp
+++ server/VContActions.cpp
@@ -1,3 +1,11 @@
+// Class to handle decoding vCont packets.
+//
+// This file is part of the Embecosm GDB Server.
+//
+// Copyright (C) 2017-2019 Embecosm Limited
+// SPDX-License-Identifier: GPL-3.0-or-later
+// ----------------------------------------------------------------------------
+
 #include "VContActions.h"
 #include "embdebug/Utils.h"
 
