1f106462fe45d91f087b1e25903c091939cb1b4b
Edward Jones <ed.jones@embecosm.com>
2019-04-02 10:55:37 +0100

    Include <algorithm> header to make std::max available.

--- server/GdbServer.cpp
+++ server/GdbServer.cpp
@@ -6,6 +6,7 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 // ----------------------------------------------------------------------------
 
+#include <algorithm>
 #include <cassert>
 #include <cstdlib>
 #include <cstring>
