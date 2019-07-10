34a73bf70b44004bd903d234b039fad727d54617
Edward Jones <ed.jones@embecosm.com>
2019-07-10 11:48:13 +0100

    Refactor of Utils.cpp and Utils.h
    * Move doxygen comments from implementation to header.
    * Put static functions in a Utils namespace instead of an
      uninstantiated Utils class
    * Add a function Utils::fatalError , to be used to report
      unrecoverable errors.

--- include/embdebug/Utils.h
+++ include/embdebug/Utils.h
@@ -15,34 +15,146 @@
 
 namespace EmbDebug {
 
-//! A class offering a number of convenience utilities for the GDB Server.
+namespace Utils {
 
-//! All static functions. This class is not intended to be instantiated.
-class Utils {
-public:
-  static bool isHexStr(const char *buf, const std::size_t len);
-  static uint8_t char2Hex(int c);
-  static char hex2Char(uint8_t d);
-  static void regVal2Hex(uint64_t val, char *buf, std::size_t numBytes,
-                         bool isLittleEndianP);
-  static uint64_t hex2RegVal(const char *buf, std::size_t numBytes,
-                             bool isLittleEndianP);
-  static std::size_t val2Hex(uint64_t val, char *buf);
-  static uint64_t hex2Val(const char *buf, std::size_t len);
-  static void ascii2Hex(char *dest, const char *src);
-  static void hex2Ascii(char *dest, const char *src);
-  static std::size_t rspUnescape(char *buf, std::size_t len);
-  static std::vector<std::string> &split(const std::string &s,
-                                         const std::string &delim,
-                                         std::vector<std::string> &elems);
+//! \brief Is this a valid hex string
+//!
+//! Checks that the first \p len characters are hex digits
+//!
+//! \param [in] buf Buffer containing >= \p len characters
+//! \param [in] len Number of characters (>0)
+//! \return True if the first \p len characters of \p buf are valid
+//!         hex digits. False otherwise.
+bool isHexStr(const char *buf, const std::size_t len);
 
-  static bool str2int(int &i, const std::string &str, int base = 0);
+//! \brief Determine the integer value of a hex digit
+//!
+//! \param[in] ch A character representing a hexadecimal digit
+//! \return The numeric value of the hex character, or -1 if the character
+//!         is invalid.
+uint8_t char2Hex(int c);
 
-private:
-  // Private constructor cannot be instantiated
+//! \brief Utility mapping a hexadecimal value to a character value
+//!
+//! \param[in] d A hexadecimal value (0-15). Any non-hex value results in
+//!              '\0' being returned.
+//! \return The lowercase character representing a given hexadecimal value,
+//!         or '\0' if an invalid value was provided.
+char hex2Char(uint8_t d);
 
-  Utils(){};
-};
+//! \brief Convert a register value to a hex digit string
+//!
+//! The supplied value is converted to a (\p numBytes * 2) digit hex string. The
+//! string is null terminated for convenience.
+//!
+//! Bytes will be packed into the string either in little or big endian
+//! order, depending on the value of /p isLittleEndianP .
+//!
+//! \param[in]  val             The value to convert
+//! \param[out] buf             The buffer for the text string. This must
+//!                             be large enough for the resultant string plus
+//!                             a null terminator.
+//! \param[in]  numBytes        The number of significant bytes in val
+//! \param[in]  isLittleEndianP True if the bytes should be written out in
+//!                             little endian order.
+void regVal2Hex(uint64_t val, char *buf, std::size_t numBytes,
+                bool isLittleEndianP);
+
+//! \brief Convert a hex digit string to a register value
+//!
+//! The supplied (\p numBytes * 2) digit hex string
+//!
+//! Bytes will be extracted from the string either in little or big
+//! endian order, depending on the value of /p isLittleEndianP
+//!
+//! \param[in] buf             The buffer with the hex string. This must
+//!                            contain (\p numBytes * 2) valid hexadecimal
+//!                            digits.
+//! \param[in] numBytes        The number of significant bytes in \p buf
+//! \param[in] isLittleEndianP True if this is a little endian architecture.
+//! \return The hexadecimal string converted to a register value.
+uint64_t hex2RegVal(const char *buf, std::size_t numBytes,
+                    bool isLittleEndianP);
+
+//! \brief Convert any non-negative value to a hex digit string
+//!
+//! The supplied value is converted to a hex string. The string is null
+//! terminated for convenience. The endianness is always big-endian (how do
+//! you do little-endian with an odd number of digits, since bytes are always
+//! big-endian).
+//!
+//! We null terminate the string.
+//!
+//! \param[in]  val  the value to convert
+//! \param[out] buf  the buffer for the text string (assumed to be large
+//!                  enough)
+//! \return The length of the hex string
+std::size_t val2Hex(uint64_t val, char *buf);
+
+//! \brief Convert a hex digit string to a general non-negative value
+//!
+//! The supplied hex-string is converted to a value. The string is null
+//! terminated for convenience. The endianness is always big-endian (how do
+//! you do little-endian with an odd number of digits, since bytes are always
+//! big-endian).
+//!
+//! \param[in] buf  The buffer with the hex string
+//! \param[in] len  The number of chars
+//! \return  The value converted
+uint64_t hex2Val(const char *buf, std::size_t len);
+
+//! \brief Convert an ASCII character string to pairs of hex digits
+//!
+//! Both source and destination are null terminated.
+//!
+//! @param[out] dest  Buffer to store the hex digit pairs (null terminated)
+//! @param[in]  src   The ASCII string (null terminated)
+void ascii2Hex(char *dest, const char *src);
+
+//! \brief Convert pairs of hex digits to an ASCII character string
+//!
+//! Both source and destination are null terminated.
+//!
+//! \param[out] dest  The ASCII string (null terminated)
+//! \param[in]  src   Buffer holding the hex digit pairs (null terminated)
+void hex2Ascii(char *dest, const char *src);
+
+//! \brief "Unescape" RSP binary data
+//!
+//! '#', '$' and '}' are escaped by preceding them by '}' and oring with 0x20.
+//!
+//! This function reverses that, modifying the data in place.
+//!
+//! \param[in] buf  The array of bytes to convert
+//! \para[in]  len   The number of bytes to be converted
+//! \return  The number of bytes AFTER conversion
+std::size_t rspUnescape(char *buf, std::size_t len);
+
+//! \brief Split a string into delimited tokens
+//!
+//! \param[in]  s      The string of tokes
+//! \param[in]  delim  The delimiter characters
+//! \param[out] elems  Vector the individual tokens
+//! \return  The vector of tokens
+std::vector<std::string> &split(const std::string &s, const std::string &delim,
+                                std::vector<std::string> &elems);
+
+//! \brief Convert a string to an integer.
+//!
+//! \param[out] i     The converted value, undefined if the conversion
+//!                   fails.
+//! \param[in]  str   The string to convert.
+//! \param[in]  base  The base for the conversion, defaults to 0, which
+//!                   means a suitable base is guessed from the string.
+//! \return Returns true if the conversion was successful, otherwise false.
+bool str2int(int &i, const std::string &str, int base = 0);
+
+//! \brief Report a fatal error to stderr and abort
+//!
+//! \param[in] error A plain-text error message to be reported
+void fatalError(std::string);
+
+} // namespace Utils
 
 } // namespace EmbDebug
 
--- server/Utils.cpp
+++ server/Utils.cpp
@@ -21,10 +21,6 @@ using std::vector;
 
 using namespace EmbDebug;
 
-//! Is this a valid hex string
-
-//! The string must be 1 or more chars and all chars must be valid hex digit.
-
 bool Utils::isHexStr(const char *buf, const std::size_t len) {
   if (len < 1)
     return false;
@@ -36,14 +32,6 @@ bool Utils::isHexStr(const char *buf, const std::size_t len) {
   return true;
 }
 
-//! Utility to give the value of a hex char
-
-//! @param[in] ch  A character representing a hexadecimal digit. Done as -1,
-//!                for consistency with other character routines, which can
-//!                use -1 as EOF.
-
-//! @return  The value of the hex character, or -1 if the character is
-//!          invalid.
 uint8_t Utils::char2Hex(int c) {
   return ((c >= 'a') && (c <= 'f'))
              ? c - 'a' + 10
@@ -52,9 +40,6 @@ uint8_t Utils::char2Hex(int c) {
                    : ((c >= 'A') && (c <= 'F')) ? c - 'A' + 10 : -1;
 }
 
-//! Utility mapping a value to hex character
-
-//! @param[in] d  A hexadecimal digit. Any non-hex digit returns a NULL char
 char Utils::hex2Char(uint8_t d) {
   static const char map[] = "0123456789abcdef"
                             "\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0"
@@ -76,18 +61,6 @@ char Utils::hex2Char(uint8_t d) {
   return map[d];
 }
 
-//! Convert a register value to a hex digit string
-
-//! The supplied value is converted to a (numBytes * 2) digit hex string. The
-//! string is null terminated for convenience.
-
-//! Rather bizarrely, GDB seems to expect the bytes in the string to be
-//! ordered according to target endianism
-
-//! @param[in]  val              the value to convert
-//! @param[out] buf              the buffer for the text string
-//! @param[in]  numBytes         the number of significant bytes in val
-//! @param[in]  isLittleEndianP  true if this is a little endian architecture.
 void Utils::regVal2Hex(uint64_t val, char *buf, std::size_t numBytes,
                        bool isLittleEndianP) {
   if (isLittleEndianP) {
@@ -113,18 +86,6 @@ void Utils::regVal2Hex(uint64_t val, char *buf, std::size_t numBytes,
   buf[numBytes * 2] = '\0'; // Useful to terminate as string
 }
 
-//! Convert a hex digit string to a register value
-
-//! The supplied (numBytes * 2) digit hex string
-
-//! Rather bizarrely, GDB seems to expect the bytes in the string to be
-//! ordered according to target endianism
-
-//! @param[in] buf              the buffer with the hex string
-//! @param[in] numBytes         the number of significant bytes in val
-//! @param[in] isLittleEndianP  true if this is a little endian architecture.
-
-//! @return  The value to convert
 uint64_t Utils::hex2RegVal(const char *buf, std::size_t numBytes,
                            bool isLittleEndianP) {
   uint64_t val = 0; // The result
@@ -143,20 +104,6 @@ uint64_t Utils::hex2RegVal(const char *buf, std::size_t numBytes,
 
   return val;
 }
-
-//! Convert any non-negative value to a hex digit string
-
-//! The supplied value is converted to a hex string. The string is null
-//! terminated for convenience. The endianness is always big-endian (how do
-//! you do little-endian with an odd number of digits, since bytes are always
-//! big-endian).
-
-//! We null terminate the string.
-
-//! @param[in]  val  the value to convert
-//! @param[out] buf  the buffer for the text string (assumed to be large
-//!                  enough)
-//! @return The length of the hex string
 
 std::size_t Utils::val2Hex(uint64_t val, char *buf) {
   int numChars = 0;
@@ -181,17 +128,6 @@ std::size_t Utils::val2Hex(uint64_t val, char *buf) {
   return strlen(buf);
 }
 
-//! Convert a hex digit string to a general non-negative value
-
-//! The supplied hex-string is converted to a value. The string is null
-//! terminated for convenience. The endianness is always big-endian (how do
-//! you do little-endian with an odd number of digits, since bytes are always
-//! big-endian).
-
-//! @param[in] buf  The buffer with the hex string
-//! @param[in] len  The number of chars
-//! @return  The value converted
-
 uint64_t Utils::hex2Val(const char *buf, std::size_t len) {
   uint64_t val = 0; // The result
 
@@ -201,12 +137,6 @@ uint64_t Utils::hex2Val(const char *buf, std::size_t len) {
   return val;
 }
 
-//! Convert an ASCII character string to pairs of hex digits
-
-//! Both source and destination are null terminated.
-
-//! @param[out] dest  Buffer to store the hex digit pairs (null terminated)
-//! @param[in]  src   The ASCII string (null terminated)                      */
 void Utils::ascii2Hex(char *dest, const char *src) {
   int i;
 
@@ -221,12 +151,6 @@ void Utils::ascii2Hex(char *dest, const char *src) {
   dest[i * 2] = '\0';
 }
 
-//! Convert pairs of hex digits to an ASCII character string
-
-//! Both source and destination are null terminated.
-
-//! @param[out] dest  The ASCII string (null terminated)
-//! @param[in]  src   Buffer holding the hex digit pairs (null terminated)
 void Utils::hex2Ascii(char *dest, const char *src) {
   int i;
 
@@ -239,16 +163,6 @@ void Utils::hex2Ascii(char *dest, const char *src) {
   dest[i] = '\0';
 }
 
-//! "Unescape" RSP binary data
-
-//! '#', '$' and '}' are escaped by preceding them by '}' and oring with 0x20.
-
-//! This function reverses that, modifying the data in place.
-
-//! @param[in] buf  The array of bytes to convert
-//! @para[in]  len   The number of bytes to be converted
-
-//! @return  The number of bytes AFTER conversion
 std::size_t Utils::rspUnescape(char *buf, std::size_t len) {
   std::size_t fromOffset = 0; // Offset to source char
   std::size_t toOffset = 0;   // Offset to dest char
@@ -269,14 +183,6 @@ std::size_t Utils::rspUnescape(char *buf, std::size_t len) {
   return toOffset;
 }
 
-//! Split a string into delimited tokens
-
-//! @param[in]  s      The string of tokes
-//! @param[in]  delim  The delimiter characters
-//! @param[out] elems  Vector the individual tokens
-
-//! @return  The vector of tokens
-
 vector<string> &Utils::split(const string &s, const string &delim,
                              vector<string> &elems) {
   elems.clear();
@@ -295,16 +201,6 @@ vector<string> &Utils::split(const string &s, const string &delim,
 
   return elems;
 }
-
-//! Convert a string to an integer.
-
-//! @param[out] i     The converted value, undefined if the conversion
-//!                   fails.
-//! @param[in]  str   The string to convert.
-//! @param[in]  base  The base for the conversion, defaults to 0, which
-//!                   means a suitable base is guessed from the string.
-
-//! @return Returns true if the conversion was successful, otherwise false.
 
 bool Utils::str2int(int &i, const std::string &str, int base) {
   const char *s;
@@ -325,4 +221,9 @@ bool Utils::str2int(int &i, const std::string &str, int base) {
 
   i = val;
   return true;
+}
+
+void Utils::fatalError(std::string str) {
+  std::cerr << "*** FATAL ERROR: " << str << std::endl;
+  abort();
 }
